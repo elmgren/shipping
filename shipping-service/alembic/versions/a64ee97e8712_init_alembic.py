b564f453d31d596785a23694139f97bb62e603a6
Marten Elmgren <elmgren@gmail.com>
2021-09-25 22:59:24 +0200

    Add alembic migration versions

---
+++ shipping-service/alembic/versions/a64ee97e8712_init_alembic.py
@@ -0,0 +1,24 @@
+"""init alembic
+
+Revision ID: a64ee97e8712
+Revises: 
+Create Date: 2021-09-25 22:41:31.054399
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a64ee97e8712'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
