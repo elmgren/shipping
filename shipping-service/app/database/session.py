106c95e23e53b51f2750c1e7f7a997a27160ef2d
Marten Elmgren <elmgren@gmail.com>
2021-09-25 22:11:28 +0200

    Add database folder and session.py for getting a session to the database
    - also add folder structure for dbo and repositories

---
+++ shipping-service/app/database/session.py
@@ -0,0 +1,31 @@
+import logging
+import os
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+logger = logging.getLogger(__name__)
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass
+
+
+def _get_url():
+    try:
+        return os.getenv("DBSTRING")
+    except KeyError as exception:
+        logger.error(f"Environment variable not set {exception}")
+        sys.exit(1)
+
+
+engine = create_engine(_get_url(), connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base
+
